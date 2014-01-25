042579ee2ec3e2c62a669d840745adc85f5fbd8b
alexgus <alex.guyon78@gmail.com>
2014-01-25 18:47:34 +0100

    - Added Communication class

---
+++ src/Com.cpp
@@ -0,0 +1,65 @@
+/*
+ * Com.cpp
+ *
+ *  Created on: 25 janv. 2014
+ *      Author: alexandre
+ */
+
+#include "Com.h"
+
+Com::Com()
+{
+	this->fd = -1;
+	this->timeout = {0,0};
+}
+
+Com::Com(int fd, timeval timeOut)
+{
+	this->fd = fd;
+	this->timeout = timeOut;
+}
+
+Com::~Com()
+{
+
+}
+
+void Com::writeString(const string &s)
+{
+	write(fd,s.c_str(),(unsigned int)s.size());
+}
+
+string& Com::readString()
+{
+	timeval t = this->timeout;
+	int ret_select;
+	fd_set readSet;
+
+	char buf[BUFFER_SIZE];
+	int nbRead = 0;
+	string *str = NULL;
+
+	ret_select = 0;
+	FD_ZERO(&readSet);
+	FD_SET(fd, &readSet);
+
+	if((ret_select = select(fd+1,&readSet,NULL,NULL,&t)) < 0)
+	{
+		log.write("Select read error : " + string(strerror(errno)),Log::ERR);
+		return *str;
+	}
+
+	// the state of fd changed
+	if(ret_select > 0)
+	{
+		// Read command
+		nbRead = read(fd,buf,BUFFER_SIZE);
+		buf[nbRead] = '\0'; // Add end of string
+
+		str = new string(buf);
+		return *str;
+	}
+
+	return *str;
+}
+
---
+++ src/Com.h
@@ -0,0 +1,73 @@
+/*
+ * Com.h
+ *
+ *  Created on: 25 janv. 2014
+ *      Author: alexandre
+ */
+
+#ifndef COM_H_
+#define COM_H_
+
+#include <time.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <string>
+#include <string.h>
+#include <errno.h>
+
+#include "Log.h"
+
+#define BUFFER_SIZE 512
+
+using namespace std;
+
+class Com
+{
+public:
+	/**
+	 * Constructor of the class
+	 */
+	Com();
+
+	/**
+	 * Constructor of the class
+	 * @param fd The file descriptor to read/write
+	 * @param timeOut The time out for the read operation
+	 */
+	Com(int fd, timeval timeOut);
+
+	/**
+	 * Destroyer of the class
+	 */
+	virtual ~Com();
+
+	/**
+	 * Write the string to the fd
+	 * @param s String to write
+	 */
+	void writeString(const string &s);
+
+	/**
+	 * Read a string from the fd
+	 * @return The string read
+	 */
+	string& readString();
+
+private:
+	/**
+	 * File descriptor to read/write
+	 */
+	int fd;
+
+	/**
+	 * The timeout for the read operation
+	 */
+	timeval timeout;
+
+	/**
+	 * The log file
+	 */
+	Log log;
+};
+
+#endif /* COM_H_ */
