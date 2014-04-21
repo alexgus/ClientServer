ce786097f491dc3712f927ff38cf93ef91994687
alexgus <alex.guyon78@gmail.com>
2014-04-21 13:33:40 +0200

    - Corrected bug byte conversion in python server

--- server.py
+++ server.py
@@ -1,3 +1,17 @@
+def help():
+	print("Help in using the python module :")
+	print("TO COMPLETE ...")
+
+def testPython():
+	print("Python module is running normally")
+	help()
+
 def recv(string):
-	print(string)
-	return 1
+	# Convert c-PyString to Python string
+	string = string.decode("utf-8")
+	
+	if string == 'TESTPY':
+		testPython()
+		return 1
+	else:
+		return -1
